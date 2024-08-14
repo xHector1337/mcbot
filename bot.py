14bfa8620c707927e3660093c3f64cfdd4c1ac05
Umut <140441119+xHector1337@users.noreply.github.com>
2024-08-14 11:55:34 +0300

    Chats random word and auto connect back function.

--- bot.py
+++ bot.py
@@ -1,12 +1,29 @@
-import javascript,random
-
+import javascript,random,json
 mineflayer = javascript.require("mineflayer")
-server = str(input("Enter the server address: "))
-port = int(input("Enter the server port: (Default port is 25565)\n"))
-version = str(input("Enter the server version: "))
-username = f"Rorschach{random.randint(0,4395837309458)}"
-bot = mineflayer.createBot({"host":server,"port":port,"username":username,"version:":version,"hideErrors":False})
 
-@javascript.On(bot,"spawn")
-def login(this):
-    print("We did that!")
+usernames = ["OyunPortall","Einshine","Herobrine","Homelander","SaulGoodman","Locked","SoldierBoy","TheDeep","ArthurMorgan","Dexter","PokerFace","0x","Rorschach","TerryDavis","KingTerry","Builderman","PunkGirl","Adrian","Umut","Hector","TheGood","TheBad","TheUgly"]
+words = ["apple","destiny","butcher","Deep the peak","dogs","cats","bird","sky","moon","sun","come","fun",""]
+
+def setup():
+    file = open("settings.json","rb")
+    data = json.load(file)
+    server = data["address"].split()
+    port = data["port"]
+    version = data["version"]
+    username = f"{random.choice(usernames)}{random.randint(0,99)}"
+    if len(data["username"]) > 1:
+        username = data["username"]
+    bot = mineflayer.createBot({"host":server,"port":port,"username":username,"version":version,"hideErrors":False,"verbose":True})
+    bot.setControlState("jump",True)
+
+    @javascript.On(bot,"spawn")
+    def login(this):
+        bot.chat(random.choice(words))
+    
+    @javascript.On(bot,"kicked")
+    def connectback(this,reason,loggedIn):
+        setup()
+           
+
+setup()
+
